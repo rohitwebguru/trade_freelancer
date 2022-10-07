d33ef4ff49c835d75133eba6441361ef157b0b8f
prathamjindal <114925577+prathamjindal@users.noreply.github.com>
2022-10-07 15:28:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ app.use(bodyParser.json());
 // Database Connection
 
 
-/*db.connect((err) => {
+db.connect((err) => {
     if (err) throw err;
     console.log('Database connected');
-})*/
+})
 
 
 const date_ob = new Date();
@@ -43,7 +43,7 @@ const day = ("0" + date_ob.getDate()).slice(-2);
 const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 const year = date_ob.getFullYear();
 const current_date = Date.parse(year + "-" + month + "-" + day);
-/*db.query("SELECT * FROM authenticattion Order by id desc LIMIT 1", function(err, result, fields) {
+db.query("SELECT * FROM authenticattion Order by id desc LIMIT 1", function(err, result, fields) {
     if (err) throw err;
     if (typeof result !== 'undefined' && result.length === 1) {
         const data = JSON.parse(result[0].data);
@@ -54,7 +54,7 @@ const current_date = Date.parse(year + "-" + month + "-" + day);
             console.log('token is not expired');
         }
     }
-});*/
+});
 
 // Set Routes
 app.use(homeRoutes.routes);
